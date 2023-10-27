89bef14b45a10614a69cde2ebb8b0972e0570855
MonstersBY <monsters.alex0@gmail.com>
2023-10-27 09:55:03 +0300

    fix: add valid

--- js/main.js
+++ js/main.js
@@ -28,3 +28,21 @@ if($('.popular-question-item').length) {
         $(this).find('.popular-question-item__body').slideToggle()
     })
 }
+
+$('.input-numbers').on( "keypress", function(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+});
